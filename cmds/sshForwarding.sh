494afdfc1d587db6ad4c257f3ef17e7f6fefeb92
giveme5 <286674783@qq.com>
2019-06-01 13:47:11 +0800

    通过ub0-hk0-vu3

--- cmds/sshForwarding.sh
+++ cmds/sshForwarding.sh
@@ -3,7 +3,8 @@
 
 for (( i = 0; i < 99999999; i++ )); do
 	#hk0也被封了，先连到ub0，再连到hk0，再连到vu3
-	ssh -v -L 18443:vu3:8443 -J root@hk0 root@vu3 sleep 99999999 #通过hk0连接到vultr
+	ssh -v -L 18443:vu3:8443 -J root@ub0,root@hk0 root@vu3 sleep 99999999 #通过ub0-hk0-vu3
+#	ssh -v -L 18443:vu3:8443 -J root@hk0 root@vu3 sleep 99999999 #hk0-vu3
 	echo "disconnected will sleep"
 	sleep 1
 done
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@
 + 在设计上采用前后端分离，pMonitorD是后端最先启动。然后用pMonitorFrame连接后端，可以单独开启、关闭某个job。
 
 # 文件
-+ pMonitorD.py，它是后端（进程容器、进程树的根）。它是通过startDeamon.sh启动的
++ pMonitorD.py，它是后端（进程容器、进程树的根）。它是通过startDeamon.sh启动的。使用pkill关闭就行，收到信号后会killall
 + pMonitorFrame.py，它是gui入口（前端）。它读取layout.json，然后创建布局。
 + statusPoller.py 每秒刷新一次状态，由pMonitorFrame启动。
 + pMonitorC.py 用于连接uds（unix domain socket）
